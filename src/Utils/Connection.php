d5bee311a9114c90899a52137b711408ba6815c6
Jorzel <jorzel@beedelivery.com.br>
2024-03-05 17:05:22 -0300

    changes to get url2

--- src/Utils/Connection.php
+++ src/Utils/Connection.php
@@ -5,9 +5,9 @@ namespace Beedelivery\Sicredi\Utils;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Client;
 
-Class Connection
+class Connection
 {
-    public function get($url, $params = null) 
+    public function get($url, $params = null)
     {
         try {
             $cliente = new Client();
@@ -18,24 +18,23 @@ Class Connection
                 'x-api-key'     => config('sicredi.x_api_key'),
                 'cooperativa'   => config('sicredi.cooperativa'), //pegar essas infor do env tbm 
                 'posto'         => config('sicredi.posto'),
-            ];   
+            ];
 
-            if(isset($params)) {
+            if (isset($params)) {
                 $response = $cliente->get(config('sicredi.base_url') . $url, [
                     'headers'     => $headerCob,
                     'json' => $params,
-                    ]);      
+                ]);
             } else {
                 $response = $cliente->get(config('sicredi.base_url') . $url, [
                     'headers'     => $headerCob,
-                    ]);      
+                ]);
             }
-            
+
             return [
                 'code' => $response->getStatusCode(),
                 'response' => json_decode($response->getBody(), true)
             ];
-
         } catch (RequestException $e) {
             return [
                 'code' => $e->getCode(),
@@ -44,7 +43,7 @@ Class Connection
         }
     }
 
-    public function getBinario($url, $params = null) 
+    public function getBinario($url, $params = null)
     {
         try {
             $cliente = new Client();
@@ -56,24 +55,22 @@ Class Connection
                 'x-api-key'     => config('sicredi.x_api_key'),
                 'cooperativa'   => config('sicredi.cooperativa'), //pegar essas infor do env tbm 
                 'posto'         => config('sicredi.posto'),
-            ];   
+            ];
 
-            if(isset($params)) {
+            if (isset($params)) {
                 $response = $cliente->get(config('sicredi.base_url') . $url, [
                     'headers'     => $headerCob,
                     'json' => $params,
-                    ]);      
+                ]);
             } else {
                 $response = $cliente->get(config('sicredi.base_url') . $url, [
                     'headers'     => $headerCob,
-                    ]);      
+                ]);
             }
-            
+
             return [
                 'code' => $response->getStatusCode(),
-                'response' => json_decode($response->getBody(), true),
-                'filename' => $response->getHeaderLine('Content-Disposition'),
-                'content' =>  $response->getBody()->getContents(),
+                'response' => $response
             ];
         } catch (RequestException $e) {
             return [
@@ -83,7 +80,7 @@ Class Connection
         }
     }
 
-    public function post($url, $params) 
+    public function post($url, $params)
     {
         try {
             $cliente = new Client();
@@ -94,18 +91,17 @@ Class Connection
                 'x-api-key'     => config('sicredi.x_api_key'),
                 'cooperativa'   => config('sicredi.cooperativa'),
                 'posto'         => config('sicredi.posto'),
-            ];   
+            ];
 
             $response = $cliente->post(config('sicredi.base_url') . $url, [
                 'headers'     => $headerCob,
                 'json' => $params,
-                ]);  
+            ]);
 
             return [
                 'code' => $response->getStatusCode(),
                 'response' => json_decode($response->getBody(), true)
             ];
-
         } catch (RequestException $e) {
             return [
                 'code' => $e->getCode(),
@@ -114,7 +110,7 @@ Class Connection
         }
     }
 
-    public function patch($url, $params, $beneficiario,$accessToken)
+    public function patch($url, $params, $beneficiario, $accessToken)
     {
         try {
             $cliente = new Client();
@@ -124,20 +120,19 @@ Class Connection
                 'Authorization' => $accessToken,
                 'codigoBeneficiario' => $beneficiario,
                 'x-api-key'     => config('sicredi.x_api_key'),
-                'cooperativa'   => config('sicredi.cooperativa'), 
+                'cooperativa'   => config('sicredi.cooperativa'),
                 'posto'         => config('sicredi.posto'),
-            ];   
+            ];
 
             $response = $cliente->patch(config('sicredi.base_url') . $url, [
                 'headers'     => $headerCob,
                 'json' => $params,
-                ]);  
+            ]);
 
             return [
                 'code' => $response->getStatusCode(),
                 'response' => json_decode($response->getBody(), true)
             ];
-
         } catch (RequestException $e) {
             return [
                 'code' => $e->getCode(),
@@ -150,31 +145,31 @@ Class Connection
     {
         try {
 
-            $certificado = base_path(config('certificate_path'));    
+            $certificado = base_path(config('certificate_path'));
             $client = new Client(['verify' => fopen($certificado, 'r'),]);
-            
+
             $headerAuth = [
                 'Content-Type' => 'application/x-www-form-urlencoded',
-                'x-api-key'=> $params['apiKey'],
+                'x-api-key' => $params['apiKey'],
                 'context' => 'COBRANCA',
             ];
 
-            if(!isset($params['refresh'])) {
+            if (!isset($params['refresh'])) {
                 $dataAuth =  [
                     'username' => $params['username'],
                     'password' => $params['password'],
-                    'scope'=> 'cobranca',
-                    'grant_type'=> 'password',
+                    'scope' => 'cobranca',
+                    'grant_type' => 'password',
                 ];
-            }else {
+            } else {
                 $dataAuth =  [
-                    'scope'=> 'cobranca',
-                    'grant_type'=> 'refresh_token',
-                    'refresh_token' => $params['refresh'], 
+                    'scope' => 'cobranca',
+                    'grant_type' => 'refresh_token',
+                    'refresh_token' => $params['refresh'],
                 ];
             }
 
-            $response = $client->request('POST' , $params['url'] . 'auth/openapi/token', [
+            $response = $client->request('POST', $params['url'] . 'auth/openapi/token', [
                 'headers'     => $headerAuth,
                 'form_params' => $dataAuth,
             ]);
@@ -183,7 +178,6 @@ Class Connection
                 'code' => $response->getStatusCode(),
                 'response' => json_decode($response->getBody(), true)
             ];
-
         } catch (RequestException $e) {
             return [
                 'code' => $e->getCode(),
